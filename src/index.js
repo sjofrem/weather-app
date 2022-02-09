3127f6117e352c99ca5e62940d68f4a816c6edfe
Sebastian Jofre <sebastian.jofrem@usm.cl>
2022-02-09 17:13:59 -0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+async function getJsonWeather(city) {
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2538e9ad7488cc7c4c9748d1aaa0987b`, {
+            mode: 'cors'
+        });
+        const cityWeather = await response.json();
+        console.log(cityWeather);
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+getJsonWeather('London');
